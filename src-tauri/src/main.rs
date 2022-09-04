e3c0fd49a2268846daad8c4661d06ae55715715b
AI1411 <goethe0719@gmail.com>
2022-09-04 21:29:18 +0900

    test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,26 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct MyMessage {
+    field_str: String,
+    field_u32: u32,
+}
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn command_with_object(message: MyMessage) -> MyMessage {
+    let MyMessage { field_str, field_u32 } = message;
+
+    MyMessage {
+        field_str: format!("{}", field_str),
+        field_u32: field_u32 + 1,
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            command_with_message
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
